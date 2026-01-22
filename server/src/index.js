4af40aa0c38a9f013bf3e6d246074405d1be0b96
Owen Powell <opowell2899@gmail.com>
2026-01-22 14:59:20 -0500

    Test auth and fix errors. Auth now functions properly

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/test', (req, res) => {
+  res.json({ message: 'Server is working!' });
+});
+
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
